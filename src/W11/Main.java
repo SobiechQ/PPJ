d1a695eeff822a0d2323446842f4564ac1b64670
Sobiech <mikolajmikolaj090302@gmail.com>
2022-12-19 12:06:55 +0100

    Dziedziczenie oraz super.

--- src/W11/Main.java
+++ src/W11/Main.java
@@ -1,7 +1,34 @@
 package W11;
 
 public class Main {
+//    int tmp;
+//    public void met(){
+//        class A{
+//            int tmp;
+//            public void met(){
+//                Main.this.tmp=7;
+//                //odwołuje sie do inta w Main
+//                this.tmp=10;
+//                //odwołuje sie do tmp należącego do klasy A.
+//
+//            }
+//        }
+//    }
     public static void main(String[] args) {
-        
+        Person person = new Person();
+        Student student = new Student();
+        System.out.println(person.toString());
+        System.out.println(student.toString());
+        //METODA to string istnieje poniważ KAŻDA KLASA DZIEDZITY PO KLASIE Object
+        Object object = new Object();
+        System.out.println(object.getClass());
+        System.out.println("===================================");
+//=================================================
+        student.drawName();
+        //w Student nie ma, jest w Person więc wykonuje sie ta z Person.
+        student.getOba();
+        person.draw();
+        student.draw();
+
     }
 }
---
+++ src/W11/Student.java
@@ -0,0 +1,20 @@
+package W11;
+
+public class Student
+        extends Person{
+    int jestWObu=20;
+    //extends Person bo po nim dziedziczy
+//    String name;
+//    int birthYear;
+    //moge usunąć więc nazwe i rok urodzenia,
+    //klasa bardziej wyspecjalizowana (klasa pochodna)
+    protected int sNumber;
+    public void getOba(){
+        System.out.println("z klasy student ("+this.jestWObu+"), Z klasy person ("+super.jestWObu+")");
+        //Super pozwala odwołać si e doo składników widocznych w jej klasie bazowej.
+    }
+    public void draw(){
+        super.draw();
+        System.out.print("Numer stud: "+this.sNumber+".");
+    }
+}
