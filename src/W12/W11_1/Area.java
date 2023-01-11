c829cbd1d05d37de5f4c5a50ede44828695b2a0d
Sobiech <mikolajmikolaj090302@gmail.com>
2023-01-11 13:41:34 +0100

    Polimorsizm

---
+++ src/W12/W11_1/Area.java
@@ -0,0 +1,24 @@
+package W12.W11_1;
+
+public class Area extends MathObject{
+    int width;
+    int height;
+    public Area(String name, int width, int height) {
+        super(name);
+        this.width = width;
+        this.height = height;
+    }
+    public String getWidthHeight(){
+        return this.width+" "+this.height;
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public void draw(){
+        System.out.println(super.getName()+" "+this.getWidthHeight());
+    }
+}
