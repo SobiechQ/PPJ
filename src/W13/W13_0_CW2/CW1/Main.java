c7aeb4dee2485ff3a724c8bcd114b49f93be5e98
Mikołaj <mikolajmikolaj090302@gmail.com>
2023-01-22 12:43:04 +0100

    MyScanner class

--- src/W13/W13_0_CW2/CW1/Main.java
+++ src/W13/W13_0_CW2/CW1/Main.java
@@ -3,7 +3,12 @@ package W13.W13_0_CW2.CW1;
 public class Main {
     public static void main(String[] args) {
         MyScanner myScanner = new MyScanner(System.in);
+        /*
 
+            PROSZE SIE TYM NIE SUGEROWAC
+            PROGRAM NAPISANY JEST TRAGICZNIE JA O TYM WIEM
+
+         */
         try {
             System.out.println(myScanner.readPosIntegerNumber());
         } catch (Exception e) {
